3acc01447560f6b46fc9465b0d2c0b373bb93fca
Marina Orlova <Klipo07@example.com>
2023-06-08 14:21:16 +0700

    Update function

---
+++ src/viselica_lib/viselica.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+void sortWord(char word[], int count);
+void countLetters(char word[], int count, char latters[], int latters_count[]);
+void playHangman(char word[], int count, char latters[], int latters_count[]);
